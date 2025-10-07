a17f66f5418daf9f1b181dac21029b8a578bf0e8
User-19fff <lolkd005@gmail.com>
2025-10-07 02:03:48 +0200

    CSS & (JS => Tisztítás)

--- éles/script.js
+++ éles/script.js
@@ -1,31 +1,18 @@
-let nrow = 12;
-let ncell = 12;
+let nRow = 12;
+let nCell = 12;
+let board = new Array(nRow);
 
-let board = new Array(nrow);
-for(let i = 0;i<nrow;i++){
-    board[i] = new Array(ncell).fill(null);
+for (let i = 0; i < nRow; i++) {
+    board[i] = new Array(nCell).fill(null);
 }
-
 
 const egeszTabla = document.getElementById("map");
-for(let i = 0;i<nrow;i++){
+
+for (let i = 0; i < nRow; i++) {
     let sor = egeszTabla.insertRow();
-    for(let j = 0;j<ncell;j++){
+
+    for (let j = 0; j < nCell; j++) { 
         let cell = sor.insertCell();
-        cell.innerHTML = board[i][j] === null ? "ures":board[i][j];
+        cell.innerHTML = board[i][j] === null ? "" : board[i][j];
     }
 }
-// for(let i = 0;i<ujsor;i++){
-//     ujsor = document.getElementById("map").insertRow(i);
-//     t[i] = new Array(ncell);
-//     for(let j = 0;j<ncell;j++){
-//         t[i][j] = newrow.insertCell(j);
-//         t[i][j].id = i*ncell+j;
-//         t[i][j].onclick = function(){myStep(this);};
-//         t[i][j].style.width = "50px";
-//         t[i][j].style.heigth = "50px";
-//         t[i][j].style.color = "#fcfcfc";
-//         t[i][j].style.background = "#ff3333";
-//         t[i][j].innerHtml = parseInt(t[i][j].id)+1;
-//     }
-// }
